6d42e160bead33ca8252549e83414a0d0350d124
Aiman Uddin <ausiaam83@gmail.com>
2021-08-24 07:57:55 +0600

    footer component is ok

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className="my-2">
             <footer className="text-center">
-                <p className="footer">Develop by <span><a target='_blank' href="https://www.facebook.com/md.siaam.3538">Aiman</a></span></p>
+                <p className="footer">Develop by <span><a target='_blank' rel="noreferrer" href="https://www.facebook.com/md.siaam.3538">Aiman</a></span></p>
             </footer>
         </div>
     );
